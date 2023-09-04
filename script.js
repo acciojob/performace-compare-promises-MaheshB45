1e3f24e3ec89ff0a5fca96a8dcfbe214d5d76518
Mahesh Rajendra Bahir <107680212+MaheshB45@users.noreply.github.com>
2023-09-04 11:09:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,4 +12,38 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
 
-// You can write your code here
+// Function to fetch data from a single API URL
+async function fetchData(apiUrl) {
+  const response = await fetch(apiUrl);
+  return await response.json();
+}
+
+// Function to measure the time taken for Promise.all() and Promise.any()
+async function comparePerformance() {
+  const startTimeAll = Date.now();
+  const promisesAll = apiUrls.map(fetchData);
+  
+  try {
+    await Promise.all(promisesAll);
+    const endTimeAll = Date.now();
+    const timeTakenAll = endTimeAll - startTimeAll;
+    document.getElementById("output-all").textContent = timeTakenAll + " ms";
+  } catch (error) {
+    console.error("Promise.all() failed:", error);
+  }
+
+  const startTimeAny = Date.now();
+  const promisesAny = apiUrls.map(fetchData);
+  
+  try {
+    await Promise.any(promisesAny);
+    const endTimeAny = Date.now();
+    const timeTakenAny = endTimeAny - startTimeAny;
+    document.getElementById("output-any").textContent = timeTakenAny + " ms";
+  } catch (error) {
+    console.error("Promise.any() failed:", error);
+  }
+}
+
+// Call the function to compare performance when the page loads
+window.addEventListener("load", comparePerformance);
